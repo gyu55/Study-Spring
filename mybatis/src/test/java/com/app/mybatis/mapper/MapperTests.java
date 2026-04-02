0966454be30a8e36b01d2ccf5a2635923bfbfc49
gyu55 <gyuho1102@gmail.com>
2026-04-02 12:46:02 +0900

    feat: Spring day02

--- mybatis/src/test/java/com/app/mybatis/mapper/MapperTests.java
+++ mybatis/src/test/java/com/app/mybatis/mapper/MapperTests.java
@@ -1,5 +1,7 @@
 package com.app.mybatis.mapper;
 
+import com.app.mybatis.domain.vo.MemberVO;
+import com.app.mybatis.domain.vo.PostVO;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,19 +16,62 @@ public class MapperTests {
     @Autowired
     private MemberMapper memberMapper;
 
+    @Autowired
+    private PostMapper postMapper;
+
 
     @Test
-    public void MapperTests() {
+    public void mapperTests() {
         log.info(timeMapper.getTime());
     }
 
     @Test
-    public void MapperTests2() {
+    public void mapperTests2() {
         log.info(timeMapper.getTime2());
     }
 
     @Test
-    public void MapperTests3() {
+    public void mapperTests3() {
         log.info(memberMapper.selectAll().toString());
     }
+
+    @Test
+    public void mapperTests4() {
+        memberMapper.select(1L).map(MemberVO::toString).ifPresent(log::info);
+    }
+
+    @Test
+    public void mapperTests5(){
+//        MemberVO memberVO = new MemberVO();
+//        memberVO.setMemberEmail("test789@gmail.com");
+//        memberVO.setMemberPassword("test123!@#");
+//        memberVO.setMemberName("장보고");
+
+        MemberVO memberVO = new MemberVO();
+        memberVO.setMemberEmail("test456@gmail.com");
+        memberVO.setMemberPassword("test123!@#");
+        memberVO.setMemberName("이순신");
+        memberMapper.insert(memberVO);
+    }
+
+    @Test
+    public void updateTests(){
+        MemberVO memberVO = new MemberVO();
+        memberVO.setId(2L);
+        memberVO.setMemberEmail("test456@gmail.com");
+        memberVO.setMemberPassword("test123!@#");
+        memberVO.setMemberName("장길동");
+
+        memberMapper.select(2L).map(MemberVO::toString).ifPresent(log::info);
+        memberMapper.update(memberVO);
+        memberMapper.select(2L).map(MemberVO::toString).ifPresent(log::info);
+    }
+
+    @Test
+    public void deleteTests(){
+        memberMapper.delete(2L);
+    }
+
+
+
 }
